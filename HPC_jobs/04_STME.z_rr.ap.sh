f2a2eca5eb9f7cd0e7cff6f90457e22fff5c93f0
Lucas Berger Munaro <lucasb4@illinois.edu>
2025-02-27 15:29:53 -0600

    HPC job

---
+++ HPC_jobs/04_STME.z_rr.ap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+#SBATCH --time=05-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem=48G
+#SBATCH --job-name=STME.zrrap
+#SBATCH --account=aces
+#SBATCH --partition=aces
+#
+# End of embedded SBATCH options
+#
+
+# Define directories
+BASE_DIR=~/MTME_ILWheat
+SCRIPT_DIR=$BASE_DIR
+JOBS_DIR=$BASE_DIR/HPC_jobs
+OUTPUT_DIR=$BASE_DIR/HPC_out
+
+# Job name
+JOB_NAME="04_STME.z_rr.ap"
+
+{
+  echo "$JOB_NAME.sh started on $(hostname) at $(date)"
+  echo ""
+  echo "========== $JOB_NAME.R =========="
+  cat $SCRIPT_DIR/$JOB_NAME.R
+} | mail -s "$JOB_NAME Started" lucasb4@illinois.edu
+
+# Run R script
+module purge
+module load R/4.4.2
+R CMD BATCH $SCRIPT_DIR/$JOB_NAME.R $OUTPUT_DIR/$JOB_NAME.out
+
+{
+  echo "$JOB_NAME.sh finished on $(hostname) at $(date)"
+  echo ""
+  echo "========== $JOB_NAME.out =========="
+  cat $OUTPUT_DIR/$JOB_NAME.out
+} | mail -s "$JOB_NAME Finished" lucasb4@illinois.edu
